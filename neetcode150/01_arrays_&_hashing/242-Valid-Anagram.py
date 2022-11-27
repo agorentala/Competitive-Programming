d7465e87d3994ce28410ee13bca2c6c401b345ce
agorentala <gorentala.anish@gmail.com>
2022-11-27 13:59:32 -0500

    242. Valid Anagram - Python3

---
+++ neetcode150/01_arrays_&_hashing/242-Valid-Anagram.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+
+
+solve = Solution()
+assert solve.isAnagram("anger", "range"), "Should be True"
