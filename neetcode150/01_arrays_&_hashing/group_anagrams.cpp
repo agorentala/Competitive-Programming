cd6eeb1201d28b11badc2df95f9992b7d715edc3
agorentala <gorentala.anish@gmail.com>
2022-11-28 00:37:36 -0500

    Optimized 49. Group Anagrams - C++

--- neetcode150/01_arrays_&_hashing/group_anagrams.cpp
+++ neetcode150/01_arrays_&_hashing/group_anagrams.cpp
@@ -1,32 +1,22 @@
 #include "string"
 #include "vector"
 #include "unordered_map"
+#include "algorithm"
 
 class Solution {
 public:
     std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string> &strs) {
         std::unordered_map<std::string, std::vector<std::string>> groups;
+        std::string key;
         for (std::string &str: strs) {
-            groups[getKey(str)].push_back(str);
+            key = str;
+            std::sort(key.begin(), key.end());
+            groups[key].push_back(str);
         }
 
         std::vector<std::vector<std::string>> ret;
         for (auto &group: groups) {
             ret.push_back(group.second);
-        }
-        return ret;
-    }
-
-private:
-    std::string getKey(std::string &str) {
-        int counter[26]{};
-        for (char &c: str) {
-            counter[c - 'a']++;
-        }
-
-        std::string ret;
-        for (int &count: counter) {
-            ret.append(std::to_string(count + 'a'));
         }
         return ret;
     }
